595eb70cdc9ef61522c21677eafcbdc84fc897d0
shanks-samir <paudyalsamir100@gmail.com>
2024-04-30 12:32:35 +0545

    Add project files.

---
+++ MagicVilla-API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_API.Data;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+           
+        }
+ 
+    }
+}
